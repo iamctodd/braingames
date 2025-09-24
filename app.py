a5de3358599cd3401a4882dc5b8fe151ca06c351
iamctodd <iamctodd@users.noreply.github.com>
2025-09-24 11:57:05 -0400

    Add missing redirect import to Flask

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, request, session
+from flask import Flask, render_template, jsonify, request, session, redirect
 from functools import wraps
 import json
 
